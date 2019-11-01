67360acfe1723bf287a032741e87fb74f61fc297
gaurav <grv.bta@gmail.com>
2019-11-01 06:12:14 +0530

    postfolio

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// initial state of menu button
+let showMenu = false;
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => {
+      item.classList.add("show");
+    });
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => {
+      item.classList.remove("show");
+    });
+    showMenu = false;
+  }
+}
